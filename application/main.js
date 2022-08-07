bcf793ec0f67a18aee5e39831d4671b3d91963f3
Amr <amrraafat17@yahoo.com>
2022-08-07 07:59:30 +0200

    update 2.0

--- application/main.js
+++ application/main.js
@@ -20,6 +20,52 @@
 // }
 
 document.body.style="text-align:center";
+let apiLink;
+let renderElements=function(result){
+  for(let i=0;i<result.length;i++){
+    let b=document.createElement('b');
+    let div=document.createElement("div");   
+    let repoName=document.createTextNode(`${i+1}-${result[i].name}`);
+    b.appendChild(repoName);
+    div.appendChild(b);
+    document.getElementById("render").appendChild(div);
+    i%2===0?div.setAttribute('style',styleSpecs1):div.setAttribute('style',styleSpecs2);
+  }
+}
+const styleSpecs1="color:green;margin:10px;background-color:#ccc; font-size:17px; padding:10px;width:400px;text-align:left;margin:auto";
+const styleSpecs2="color:black;margin:10px;background-color:#eee; font-size:17px; padding:10px;width:400px;text-align:left;margin:auto";
+let renderInDom= async(apilink)=>{
+  const result= await getData(apiLink);
+  console.log(result);
+  if( document.getElementById("render").innerHTML==""){
+     renderElements(result);
+  }else{
+    document.getElementById("render").innerHTML="";
+    renderElements(result);
+  }
+};
+
+let submitBtn=document.getElementById('submit');
+submitBtn.addEventListener("click",function(e){
+   e.preventDefault();
+   //get value entered in the input tag
+  let inputVal=document.getElementById('github-username').value;
+  console.log(inputVal.length);
+  console.log(inputVal);
+  if(inputVal!=="" && inputVal.length>=4){
+    console.log("authentication passed");
+    apiLink=`https://api.github.com/users/${inputVal}/repos`;
+    renderInDom(apiLink);
+  }else{
+    //show user error message!
+    console.log("username incorrect!")
+  }
+   //test value for user stupid mistakes
+   // adjust apilink with the uservalue
+});
+
+
+
   const getData=(apiLink)=>{
     return myPromise=new Promise((resolve,reject)=>{
         let myRequest=new XMLHttpRequest();
@@ -38,24 +84,6 @@ document.body.style="text-align:center";
 
   };
 
-// getData("https://api.github.com/users/amrelbardini/repos").then((result)=>{console.log(result)});
-const styleSpecs1="color:green;margin:10px;background-color:#ccc; font-size:17px; padding:10px;width:400px;text-align:left;margin:auto";
-const styleSpecs2="color:black;margin:10px;background-color:#eee; font-size:17px; padding:10px;width:400px;text-align:left;margin:auto";
-let renderInDom= async(apilink)=>{
-  const result= await getData(apiLink).then((result)=>{return result});
-  console.log(result);
-  for(let i=0;i<result.length;i++){
-                let strong=document.createElement('b');
-                let div=document.createElement("div");   
-                let repoName=document.createTextNode(`${i+1}-${result[i].full_name}`);
-                strong.appendChild(repoName);
-                div.appendChild(strong);
-                document.body.appendChild(div);
-               
-                i%2===0?div.setAttribute('style',styleSpecs1):div.setAttribute('style',styleSpecs2);
-            }
 
 
-}
-const apiLink="https://api.github.com/users/amrelbardini/repos"
-renderInDom(apiLink);
+
